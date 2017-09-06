56a224bc6312af47dbfe6d1f914b04516fdacd7c
aakp10 <karunagrewal98@gmail.com>
2017-09-06 14:42:23 +0530

    minor fix to sql output func

---
+++ output/outputSql2.js
@@ -0,0 +1,30 @@
+const Promise = require("bluebird");
+const mysql=require("mysql");
+
+var insMarks=function(content,tableName,headers){
+    var insertMarks=
+        `INSERT INTO ${tableName}  (${headers[0]},${headers[1]} VALUES ? ` ;
+     connection.query(insertMarks, [content],function(err, rows, fields)
+                                        {if(err) console.error(err.message);
+                                        });
+                                      }
+var outputTable=function(database,content,tableName){
+  var connection =
+      mysql.createConnection({
+          connectionLimit : 100,
+          host: "localhost",
+          port: "3306",
+          user: database.user,
+          password: database.password,
+          database: database.db
+      });
+      var headers=content.shift();
+      var sql = `CREATE TABLE ${tableName} IF NOT EXISTS (${headers[0]} VARCHAR(12),${headers[1]} INT`;
+      connection.queryAsync(sql)
+            .catch(err => { console.error(err);
+      insMarks(content,tableName,headers);
+
+
+
+
+}
