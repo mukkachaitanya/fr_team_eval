6c70506e7d436ddacc858998267113c2af02da3b
aakp10 <karunagrewal98@gmail.com>
2017-09-06 14:51:34 +0530

    minor fix to sql output func

--- output/outputSql.js
+++ output/outputSql.js
@@ -3,11 +3,11 @@ const mysql=require("mysql");
 
 var insMarks=function(content,tableName,headers){
     var insertMarks=
-        `INSERT INTO ${tableName}  (${headers[0]},${headers[1]} VALUES ? ` ;
+        `INSERT INTO ${tableName}  (${headers[0]},${headers[1]}) VALUES ? ` ;
      connection.query(insertMarks, [content],function(err, rows, fields)
                                         {if(err) console.error(err.message);
-                                        }
-}
+                                        });
+                                      }
 var outputTable=function(database,content,tableName){
   var connection =
       mysql.createConnection({
